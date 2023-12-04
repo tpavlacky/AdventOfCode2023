9f54eca1bcffc98207d762a4010a4a0a11d542ed
Tomas Pavlacky <tomas.pavlacky@siemens.com>
2023-12-04 10:32:23 +0100

    Day4 - Part2

--- AdventOfCode2023/Day4/Program.cs
+++ AdventOfCode2023/Day4/Program.cs
@@ -6,11 +6,20 @@ namespace Day4
 
   internal class Program
   {
+    //internal static string _demoData = """
+    //  Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    //  Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    //  Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    //  Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    //  Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    //  Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    //  """;
+
     static void Main()
     {
-      Console.WriteLine("--== PART ONE ==--");
       var cards = ToCards(_input).ToList();
 
+      Console.WriteLine("--== PART ONE ==--");
       var result = 0;
       foreach (var card in cards)
       {
@@ -18,6 +27,45 @@ namespace Day4
       }
 
       Console.WriteLine("Result: " + result);
+      Console.WriteLine("--== PART TWO ==--");
+
+      var cardsMemory = new Dictionary<int, int>();
+      foreach (var card in cards)
+      {
+        if(cardsMemory.TryGetValue(card.Nr, out var currentCardCout))
+        {
+          cardsMemory[card.Nr] = currentCardCout + 1;
+        }
+        else
+        {
+          cardsMemory[card.Nr] = 1;
+        }
+
+        for (int i = 0; i < cardsMemory[card.Nr]; i++)
+        {
+          var winningNumbersCount = card.WinningNumbers.Where(card.CardNumbers.Contains).Count();
+          if (winningNumbersCount == 0)
+          {
+            break;
+          }
+
+          var lastCopiedCardNr = card.Nr + winningNumbersCount;
+          for (int copiedCard = 1; copiedCard <= winningNumbersCount; copiedCard++)
+          {
+            var copyCardNr = card.Nr + copiedCard;
+            if (cardsMemory.TryGetValue(copyCardNr, out var cardsCount))
+            {
+              cardsMemory[copyCardNr] = cardsCount + 1;
+            }
+            else
+            {
+              cardsMemory[copyCardNr] = 1;
+            }
+          }
+        }
+      }
+
+      Console.WriteLine("Result: " + cardsMemory.Values.Sum());
     }
 
     private static IEnumerable<Card> ToCards(string input)
